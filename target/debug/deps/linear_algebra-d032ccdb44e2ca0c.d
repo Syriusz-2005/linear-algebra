ede34bf9432eb4bf806f09520c2c1710ec0d15ec
Mateusz Słomka <slomkam7@gmail.com>
2025-05-07 08:56:31 +0200

    fix: remove target from git

--- target/debug/deps/linear_algebra-d032ccdb44e2ca0c.d
+++
@@ -1,5 +0,0 @@
-/home/mateusz/studies/linear-algebra/target/debug/deps/liblinear_algebra-d032ccdb44e2ca0c.rmeta: src/main.rs
-
-/home/mateusz/studies/linear-algebra/target/debug/deps/linear_algebra-d032ccdb44e2ca0c.d: src/main.rs
-
-src/main.rs:
--- target/debug/linear-algebra.d
+++
@@ -1,1 +0,0 @@
-/home/mateusz/studies/linear-algebra/target/debug/linear-algebra: /home/mateusz/studies/linear-algebra/src/main.rs
