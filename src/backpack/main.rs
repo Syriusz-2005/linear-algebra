6546039db2aec1d0bb2140d3eeccc91a2394adc9
Mateusz Słomka <mateusz@fhub.pl>
2025-05-14 19:53:45 +0200

    feat: arc instead of copying

--- src/backpack/main.rs
+++ src/backpack/main.rs
@@ -1,4 +1,4 @@
-use std::rc::Rc;
+use std::sync::Arc;
 use std::thread::{self, JoinHandle};
 use std::{
     time::{SystemTime, UNIX_EPOCH},
@@ -9,37 +9,6 @@ use std::{
 struct Item {
     size: u64,
     value: u64,
-}
-
-fn solve_backpack_v1(items: &Vec<Item>, capacity: u64) -> Vec<&Item> {
-    let iterations = 2u64.pow(items.len() as u32) - 1;
-    let mut max_value: u64 = u64::MIN;
-    let mut winning_configuration: u64 = 0;
-    for i in 0..iterations {
-        let mut size_sum: u64 = 0;
-        let mut value_sum: u64 = 0;
-        for bit_index in 0..items.len() {
-            let mask = 2u64.pow(bit_index as u32);
-            let bit_value = i & mask;
-            if bit_value > 0 {
-                size_sum += items[bit_index].size;
-                value_sum += items[bit_index].value;
-            }
-        }
-        if size_sum <= capacity && max_value < value_sum {
-            max_value = value_sum;
-            winning_configuration = i;
-        }
-    }
-    let mut output: Vec<&Item> = Vec::new();
-    for bit_index in 0..items.len() {
-        let mask = (2 as u64).pow(bit_index as u32);
-        let bit_value = winning_configuration & mask;
-        if bit_value > 0 {
-            output.push(&items[bit_index]);
-        }
-    }
-    return output;
 }
 
 fn solve_backpack_v2(items: &Vec<Item>, capacity: u64) -> Vec<&Item> {
@@ -77,35 +46,6 @@ fn solve_backpack_v2(items: &Vec<Item>, capacity: u64) -> Vec<&Item> {
     return output;
 }
 
-fn solve_backpack_v3(items: &Vec<Item>, capacity: u64) -> Vec<&Item> {
-    let iterations = 2u64.pow(items.len() as u32) - 1;
-    let mut max_value: u64 = u64::MIN;
-    let mut winning_configuration: u64 = 0;
-    for i in 0..iterations {
-        let mut size_sum: u64 = 0;
-        let mut value_sum: u64 = 0;
-        for bit_index in 0..items.len() {
-            let bit_value = (i >> bit_index) & 1u64;
-            if bit_value > 0 {
-                size_sum += items[bit_index].size;
-                value_sum += items[bit_index].value;
-            }
-        }
-        if size_sum <= capacity && max_value < value_sum {
-            max_value = value_sum;
-            winning_configuration = i;
-        }
-    }
-    let mut output: Vec<&Item> = Vec::with_capacity(items.len());
-    for bit_index in 0..items.len() {
-        let bit_value = (winning_configuration >> bit_index) & 1u64;
-        if bit_value > 0 {
-            output.push(&items[bit_index]);
-        }
-    }
-    return output;
-}
-
 #[derive(Clone, Copy)]
 struct ComputationResult {
     winning_configuration: u64,
@@ -114,11 +54,12 @@ struct ComputationResult {
 
 fn solve_backpack_multithreaded(items: &Vec<Item>, capacity: u64) -> Vec<&Item> {
     let iterations = 2u64.pow(items.len() as u32) - 1;
-    let threads_count = 12;
+    let threads_count = 20;
     let batch_size = iterations / threads_count;
-    let thread_handles = (0..threads_count)
+    let items_arc = Arc::new(items.clone());
+    let winning_configuration = (0..threads_count)
         .map(|thread_index| {
-            let items = items.clone();
+            let items = Arc::clone(&items_arc);
             let start_i = thread_index * batch_size;
             let end_i = start_i + batch_size;
             let thread_handle = thread::spawn(move || -> ComputationResult {
@@ -147,9 +88,7 @@ fn solve_backpack_multithreaded(items: &Vec<Item>, capacity: u64) -> Vec<&Item> 
             println!("Thread created");
             return thread_handle;
         })
-        .collect::<Vec<JoinHandle<ComputationResult>>>();
-
-    let winning_configuration = thread_handles
+        .collect::<Vec<JoinHandle<ComputationResult>>>()
         .into_iter()
         .map(|thread_handle| thread_handle.join().unwrap())
         .inspect(|thread| println!("Thread completed, best: {}", thread.value_sum))
